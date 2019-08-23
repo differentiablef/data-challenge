75c3a21b44a18fa0371edfbf64771b31a6f9642d
Michael Blackmon <differentiablef@gmail.com>
2019-08-23 03:21:19 -0400

    web-app update

--- SQL-Climate/web-app.py
+++ SQL-Climate/web-app.py
@@ -4,29 +4,29 @@ from flask_api import FlaskAPI, status, exceptions
 
 app = FlaskAPI(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def notes_list():
     """
     List routes.
     """
-    return []
+    return ['node-list']
 
 @app.route('/api/v1.0/precipitation', methods=['GET'])
 def precipitation():
-    return []
+    return ['precip']
 
 @app.route('/api/v1.0/stations', methods=['GET'])
 def stations():
-    return []
+    return ['stations']
 
 @app.route('/api/v1.0/tobs', methods=['GET'])
 def tempurature():
-    return []
+    return ['temp_obs']
 
 @app.route('/api/v1.0/<start>', methods=['GET'], strict_slashes=False)
 @app.route('/api/v1.0/<start>/<end>', methods=['GET'], strict_slashes=False)
 def range_summary(start, end=None):
-    return []
+    return ['range_summary']
 
 if __name__ == "__main__":
     app.run(debug=True)
