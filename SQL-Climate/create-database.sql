77d248c0d6c72098c0b5c7e346341fb3641ca24b
Michael Blackmon <differentiablef@gmail.com>
2019-08-24 13:01:40 -0400

    updated sql-climate

--- SQL-Climate/create-database.sql
+++ SQL-Climate/create-database.sql
@@ -3,8 +3,7 @@ drop database if exists hawaii;
 create database hawaii;
 
 -- connect to newly initialized database
-\connect hawaii postgres localhost
-
+\connect hawaii postgres 
 
 -- create tables
 drop table if exists measurement;
@@ -12,17 +11,19 @@ create table measurement(
          id SERIAL PRIMARY KEY,
          station VARCHAR,
          date DATE,
-         precip DECIMAL,
-         temp_obs DECIMAL);
+         precip real,
+         temp_obs real);
        
 drop table if exists station;
 create table station(
          station VARCHAR PRIMARY KEY,
          name VARCHAR,
-         latitude DECIMAL,
-         longitude DECIMAL,
-         elevation DECIMAL);
+         latitude real,
+         longitude real,
+         elevation real);
 
 -- copy the contents of the provided csv files into the tables
 \copy station from './Resources/hawaii_stations.csv' delimiter ',' csv header;
 \copy measurement(station, date, precip, temp_obs) from './Resources/hawaii_measurements.csv' delimiter ',' csv header;
+
+
--- SQL-Climate/web-app.py
+++ SQL-Climate/web-app.py
@@ -8,8 +8,11 @@ from sqlalchemy import create_engine, func, desc
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
 
+import datetime as dt
+
 # ##############################################################################
 
+# setup flask app
 app = FlaskAPI(__name__)
 
 # setup engine for postgresql 
@@ -38,24 +41,37 @@ def notes_list():
     return {'end-points':['/api/v1.0/precipitation',
                           '/api/v1.0/stations',
                           '/api/v1.0/tobs',
-                          '/api/v1.0/<start-date>',
-                          '/api/v1.0/<start-date>/<end-date>']}
+                          '/api/v1.0/<start-date:MM-DD-YYYY>',
+                          '/api/v1.0/<start-date:MM-DD-YYYY>/<end-date:MM-DD-YYYY>']}
 
 @app.route('/api/v1.0/precipitation', methods=['GET'])
 def precipitation():
-    return ['precip']
+    results = session.query( Measurement ).all()
+    tmp = dict([
+        (
+    return results
 
 @app.route('/api/v1.0/stations', methods=['GET'])
 def stations():
-    return ['stations']
+    results = session.query( Station ).all()
+    return  { 'stations':
+              [ [ result.station, result.name,
+                  result.latitude, result.longitude, result.elevation ] \
+                        for result in results ] }
 
 @app.route('/api/v1.0/tobs', methods=['GET'])
 def tempurature():
-    return ['temp_obs']
+    results = session.query( Measurement.date, Measurement.temp_obs ).all()
+    return results
 
 @app.route('/api/v1.0/<start>', methods=['GET'], strict_slashes=False)
 @app.route('/api/v1.0/<start>/<end>', methods=['GET'], strict_slashes=False)
 def range_summary(start, end=None):
+    
+    begin_date = dt.datetime.strptime(start, "%m-%d-%Y")
+    if end:
+        end_date = dt.datetime.strptime(end, "%m-%d-%Y")
+        
     return ['range_summary']
 
 # ##############################################################################
