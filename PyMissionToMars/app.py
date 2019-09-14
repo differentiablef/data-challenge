597906c4f5efd4b04859edc3e19c423f94096643
Michael Blackmon <differentiablef@gmail.com>
2019-09-13 23:48:10 -0400

    finalize flask app

---
+++ PyMissionToMars/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import pymongo
+
+app = Flask(__name__)
+
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+@app.route('/')
+def main():
+    info = client.marsdb.front_page.find()
+    return render_template('index.html', info=info[-1])
+
+
+@app.route('/scrape')
+def do_scrape():
+    from scrape_mars import scrape
+    out = scrape()
+    client.marsdb.front_page.insert_one( out )
+    return "done."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
